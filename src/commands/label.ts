0dffa096a89cd289abddfa00f2c1a629a3079153
Aaron Dewes <aaron.dewes@web.de>
2021-04-27 21:23:26 +0200

    Fix

--- src/commands/label.ts
+++ src/commands/label.ts
@@ -3,21 +3,23 @@ import { addLabel, labelExists } from '../helpers';
 import Command from './command';
 
 export default class CmdLabel extends Command {
-    // TODO: Remove this when Prettier supports override
-    // eslint-disable-next-line
-    static helptext = "This adds a label to the current PR/issue.";
+    static helptext = 'This adds a label to the current PR/issue.';
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     static run(context: Context, args: string, _isPR: boolean): void {
         if (labelExists(context.octokit, context.repo().owner, context.repo().repo, args)) {
-            addLabel(
-                context.octokit,
-                context.repo().owner,
-                context.repo().repo,
-                context.issue().issue_number,
-                args,
-                '',
-            );
+            try {
+                addLabel(
+                    context.octokit,
+                    context.repo().owner,
+                    context.repo().repo,
+                    context.issue().issue_number,
+                    args,
+                    '',
+                );
+            } catch {
+                context.octokit.issues.createComment({ ...context.issue(), body: `This label could not be found.` });
+            }
         } else {
             context.octokit.issues.createComment({ ...context.issue(), body: `This label could not be found.` });
         }
